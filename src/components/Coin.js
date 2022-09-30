e5e5c89a20cfed77818e370da8409d2592099703
meshalmali <shalmalib15@gmail.com>
2022-09-30 10:25:08 +0530

    Responsiveness

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,18 +5,32 @@ function Coin({ name, image, symbol, volume, price, priceChange }) {
   return (
     <div className="coin">
       <div className="coin_row">
-        <img src={image} alt="" />
-        <h1>{name}</h1>
-        <p className="coin_symbol">{symbol}</p>
+        <div className="coin_details">
+          <div className="coin_img">
+            <img src={image} alt="" />
+          </div>
+          <div className="coin_name">
+            <h1>{name}</h1>
+          </div>
+          <div className="coin_symbol">
+            <p className="coin_symbol">{symbol}</p>
+          </div>
+        </div>
 
         <div className="coin_data">
-          <p className="coin_price">INR {price}</p>
-          <p className="coin_volume">INR {volume.toLocaleString()}</p>
-          {priceChange < 0 ? (
-            <p className="coin_percent_red">{priceChange.toFixed(2)}%</p>
-          ) : (
-            <p className="coin_percent_green">{priceChange.toFixed(2)}%</p>
-          )}
+          <div className="coin_price">
+            <p className="coin_price">INR {price}</p>
+          </div>
+          <div className="coin_vol">
+            <p className="coin_volume">INR {volume.toLocaleString()}</p>
+          </div>
+          <div className="coin_percent">
+            {priceChange < 0 ? (
+              <p className="coin_percent_red">{priceChange.toFixed(2)}%</p>
+            ) : (
+              <p className="coin_percent_green">{priceChange.toFixed(2)}%</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
